93dee92431c5aa3ddfe4d64c7a68a28e57153882
Matthias Mueller <matthiasm@live.de>
2019-03-01 22:08:43 +0100

    #341 Added Console Command to provoke Exception

---
+++ Sources/TestConsole/Areas/ConsoleCommands/ThrowException.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Mmu.Mlazh.AzureApplicationExtensions.Areas.AzureFunctions.Context;
+using Mmu.Mlazh.AzureApplicationExtensions.TestConsole.Areas.Services;
+using Mmu.Mlh.ConsoleExtensions.Areas.Commands.Models;
+using Mmu.Mlh.ConsoleExtensions.Areas.ConsoleOutput.Services;
+
+namespace Mmu.Mlazh.AzureApplicationExtensions.TestConsole.Areas.ConsoleCommands
+{
+    public class ThrowException : IConsoleCommand
+    {
+        private readonly IConsoleWriter _consoleWriter;
+
+        public ThrowException(IConsoleWriter consoleWriter)
+        {
+            _consoleWriter = consoleWriter;
+        }
+
+        public async Task ExecuteAsync()
+        {
+            var actionResult = await AzureFunctionExecutionContext.ExecuteAsync<ITestService>(
+                testService => throw new Exception("Hello Exception!"));
+
+            var objectResult = (ObjectResult)actionResult;
+            _consoleWriter.WriteLine(objectResult.Value.ToString(), null, ConsoleColor.Green);
+        }
+
+        public string Description { get; } = "Throw Exception";
+        public ConsoleKey Key { get; } = ConsoleKey.D3;
+    }
+}
